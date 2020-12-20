41c71b4e80b8c73e6bc0706becadeda2f6f25937
awrico <awrico@xaltius.tech>
2020-12-20 21:29:50 +0700

    Update homepage

---
+++ precache-manifest.fb43739ac9a9a92faa9ece885493e2cb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d3babe7e8141f5d1a5a234b7d639af",
+    "url": "/taarufin/index.html"
+  },
+  {
+    "revision": "5d95b2becaec1f2cdb4b",
+    "url": "/taarufin/static/css/main.62078aa7.chunk.css"
+  },
+  {
+    "revision": "1f54d82f4860e213cb43",
+    "url": "/taarufin/static/js/2.4aede882.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/taarufin/static/js/2.4aede882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d95b2becaec1f2cdb4b",
+    "url": "/taarufin/static/js/main.adf28b30.chunk.js"
+  },
+  {
+    "revision": "a93891944b15d9542743",
+    "url": "/taarufin/static/js/runtime-main.bd4c1a6e.js"
+  },
+  {
+    "revision": "647f8ae0926c1883902321deecc51c6f",
+    "url": "/taarufin/static/media/Bowlist.647f8ae0.ttf"
+  },
+  {
+    "revision": "dc93555ed379e1547d3f0b9b1450c337",
+    "url": "/taarufin/static/media/Skia.dc93555e.ttf"
+  },
+  {
+    "revision": "79c510dd6f9d8c8f87645bedc2a55832",
+    "url": "/taarufin/static/media/cv.79c510dd.png"
+  },
+  {
+    "revision": "878b1f275a7ce0b6db3cc9f350f34e37",
+    "url": "/taarufin/static/media/desert.878b1f27.png"
+  },
+  {
+    "revision": "22f6d90ffdca715d3b550ec467878b82",
+    "url": "/taarufin/static/media/image 2.22f6d90f.jpg"
+  },
+  {
+    "revision": "f787f510e21e88a90b55fd679d22a578",
+    "url": "/taarufin/static/media/logo.f787f510.png"
+  },
+  {
+    "revision": "fd49b3ce8ae1d44bf5038d41d4886345",
+    "url": "/taarufin/static/media/man.fd49b3ce.png"
+  },
+  {
+    "revision": "b7f242d6b4bb05d56be38a20ff5f7242",
+    "url": "/taarufin/static/media/muslimah.b7f242d6.jpg"
+  },
+  {
+    "revision": "14984783e0bace1beb1095d5b4acac99",
+    "url": "/taarufin/static/media/register.14984783.png"
+  },
+  {
+    "revision": "e4dfbb722d7a3f705b972f530a88b33b",
+    "url": "/taarufin/static/media/sky.e4dfbb72.png"
+  },
+  {
+    "revision": "da40370938f531bdd9a2a389f8c2055e",
+    "url": "/taarufin/static/media/sky_moon.da403709.png"
+  },
+  {
+    "revision": "0bf378d13c7a8ebe1531c55a18e9aeb4",
+    "url": "/taarufin/static/media/woman.0bf378d1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc8bdb6682023cbc790f4c9927f5b01b.js"
+  "/taarufin/precache-manifest.fb43739ac9a9a92faa9ece885493e2cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taarufin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
